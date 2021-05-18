1babc638ab7c40ae1e6caaa82dcf836be6369daa
Ben Daniels <benbanj@hotmail.com>
2021-05-18 19:59:38 +0100

    Styling form

--- library.js
+++ library.js
@@ -28,44 +28,63 @@ function addBook(e) {
   form.reset();
 }
 
+function bookCardBody(book) {
+  const cardBody = document.createElement('div');
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('h5');
+  const pages = document.createElement('p');
+  const read = document.createElement('p');
+
+  bookTitle.textContent = `Title: ${book.title}`;
+  bookAuthor.textContent = `Author: ${book.author}`;
+  pages.textContent = `Pages: ${book.pages}`;
+  read.textContent = `Read: ${book.read}`;
+  cardBody.append(bookTitle, bookAuthor, pages, read);
+  cardBody.classList.add('card-body');
+
+  return cardBody;
+}
+
+function bookCardFooter(book) {
+  const cardFooter = document.createElement('div');
+  const removeButton = document.createElement('button');
+  const readCheckbox = document.createElement('input');
+
+  readCheckbox.type = "checkbox";
+  readCheckbox.name = "readCheckbox";
+  readCheckbox.id = "readCheckbox"; 
+  readCheckbox.required = true; 
+  if (book.read === 'yes') readCheckbox.checked = true;
+  readCheckbox.addEventListener('change', toggleRead);
+
+  removeButton.textContent = "Remove";
+  removeButton.addEventListener('click', removeBook);
+
+  cardFooter.classList.add('card-footer');
+  cardFooter.append(removeButton);
+  cardFooter.append(readCheckbox);
+
+  return cardFooter;
+}
+
+function bookCard(book, index) {
+    const card = document.createElement('div');
+    const cardBody = bookCardBody(book);
+    const cardFooter = bookCardFooter(book);
+
+    card.setAttribute('data-index', index);
+    card.classList.add('card');
+    card.append(cardBody, cardFooter);
+
+    return card;
+}
+
 function displayBooks() {
   const container = document.querySelector('.books-container');
   container.innerHTML = null;
 
   library.forEach((book, index) => {
-    const card = document.createElement('div');
-    const cardBody = document.createElement('div');
-    const cardFooter = document.createElement('div');
-    const bookTitle = document.createElement('h3');
-    const bookAuthor = document.createElement('h5');
-    const pages = document.createElement('p');
-    const read = document.createElement('p');
-    const removeButton = document.createElement('button');
-    const readCheckbox = document.createElement('input');
-
-    card.setAttribute('data-index', index);
-    readCheckbox.type = "checkbox";
-    readCheckbox.name = "readCheckbox";
-    readCheckbox.id = "readCheckbox"; 
-    readCheckbox.required = true; 
-    card.classList.add('card');
-    cardBody.classList.add('card-body');
-    cardFooter.classList.add('card-footer');
-    card.append(cardBody, cardFooter);
-    cardBody.append(bookTitle, bookAuthor, pages, read);
-    cardFooter.append(removeButton);
-    cardFooter.append(readCheckbox);
-    if (book.read === 'yes') {
-      readCheckbox.checked = true;
-    }
-    bookTitle.textContent = `Title: ${book.title}`;
-    bookAuthor.textContent = `Author: ${book.author}`;
-    pages.textContent = `Pages: ${book.pages}`;
-    read.textContent = `Read: ${book.read}`;
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener('click', removeBook);
-    readCheckbox.addEventListener('change', toggleRead)
-    
+    const card = bookCard(book, index);    
     container.append(card);
   });
 }
